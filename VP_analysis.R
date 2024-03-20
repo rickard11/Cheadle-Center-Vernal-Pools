3f6a7f5ec6c3effbeedf18c1464535b7dc568815
rickard11 <rickard@ucsb.edu>
2024-03-20 15:57:17 -0700

    added scatter plots

--- VP_analysis.R
+++ VP_analysis.R
@@ -1,20 +1,24 @@
 install.packages("lubridate")
 library(lubridate)
+library(ggplot2)
+library(dplyr)
 #First upload rain data and get max and total
-noaa<-read.csv("VP_hydrology/Monthly_NOAA.csv")
-noaayrly<-aggregate(cbind(Precip_mm,AVG_Temp_C)~Rain.Year,noaa,FUN=max)
+noaa<-read.csv("Monthly_NOAA.csv")
+noaayrly<-aggregate(Precip_mm~Rain.Year,noaa,FUN=sum)
 noaayrly<-noaayrly[noaayrly$Rain.Year>=2019,]
 noaayrly$wtr_yr<-c("WY19","WY20","WY21","WY22","WY23","WY24")
 
 #Load clean vp data
-vp<-read.csv("VP_hydrology/vp_clean.csv")
+vp<-read.csv("vp_clean.csv")
 vpq<-vp[vp$Water_Level!=0,]
-vpmax
+#############################
+##Temperary until 2024 water year is over
+vpq<-vpq[vpq$wtr_yr!="WY24",]
 vpq$days<-7
 vpqag<-aggregate(days~Location+VP_Name+wtr_yr,vpq,FUN=sum)
 vpmax<-aggregate(Water_Level~Location+VP_Name+wtr_yr,vp,FUN=max)
 vpqag
-head(vpq)head(vpq)sum()
+
 
 
 vpall<-merge(vpqag,noaayrly,by="wtr_yr",all.x=TRUE,all.y=TRUE)
@@ -24,6 +28,38 @@ coeff<-0.2
 vpall$Precip_in<-vpall$Precip_mm/25.4
 vpall$Precip_cm<-vpall$Precip_mm/10
 lubridate::ymd(vpall$Rain.Year, truncated = 2L)
+
+coeff<-0.5
+ggplot(vpall,aes(x=Rain.Year)) +
+  geom_bar(stat="identity", position=position_dodge(),aes(y=days, fill=VP_Name))+ylab("Total Days Inundated")+xlab("Vernal Pool Name")+ 
+  scale_y_continuous(limits=c(0,250),name = "First Axis",sec.axis = sec_axis( trans=~.*coeff, name="Second Axis"))+
+  theme_bw()+geom_line(color="lightblue",size=1.5,aes(y=Precip_cm/coeff))+geom_point(aes(y=Precip_cm/coeff))+
+theme(axis.title.y = element_text(size=18), 
+      axis.title.y.right = element_text(size=15),
+      axis.title.x=element_text(size=18), axis.text.x=element_text(size=18),
+      axis.text.y = element_text(size=18),title =element_text(size=18, face='bold'),
+      strip.text.y = element_text(size = 18),
+      legend.text = element_text(size=12),
+      legend.title = element_text(size=12),
+      legend.position = "right")+ggtitle(p$Location)
+
+for (i in unique(vpall$Location)){
+  p<-vpall[vpall$Location==i,]
+  q<- ggplot(p,aes(x=Rain.Year)) +
+    geom_bar(stat="identity", position=position_dodge(),aes(y=days, fill=VP_Name))+ylab("Total Days Inundated")+xlab("Vernal Pool Name")+ 
+    scale_y_continuous( name = "Water Year Rainfall (Inches)")+
+  theme_bw()+geom_line(color="lightblue",size=1.5,aes(y=Precip_cm/coeff))+geom_point(aes(y=Precip_cm))+
+    theme(axis.title.y = element_text(size=18,margin = margin(t = 0, r = 10, b = 0, l = 20)), 
+          axis.title.y.right = element_text(size=15,margin = margin(t = 0, r = 0, b = 0, l = 20)),
+          axis.title.x=element_text(size=18), axis.text.x=element_text(size=18),
+          axis.text.y = element_text(size=18),title =element_text(size=18, face='bold'),
+          strip.text.y = element_text(size = 18),
+          legend.text = element_text(size=12),
+          legend.title = element_text(size=12),
+          legend.position = "right")+ggtitle(p$Location)+
+    guides(fill=guide_legend(ncol=2))
+  print(q)
+}
 
 for (i in unique(vpall$Location)){
   p<-vpall[vpall$Location==i,]
@@ -63,7 +99,73 @@ for (i in unique(vpmax$Location)){
 
 
 
+vpall
+vpmax
+head(vpall)
+head(vpmax)
+vp2<-merge(vpall,vpmax,by=c("Location","VP_Name","wtr_yr"))
+vp2<-vp2[vp2$Water_Level<100,]
+vp3max<-aggregate(cbind(days,Water_Level)~Location,vp2,FUN=mean)
+dsvp<-vp2[vp2$Location=="delsol_caminocorto",]
+
+plot(vp2$Water_Level~vp2$days)
+plot(dsvp$Water_Level~dsvp$days)
+
+for (i in unique(vp2$wtr_yr)){
+  p<-vp2[vp2$wtr_yr==i,]
+  q<- ggplot(p,aes(x=Water_Level)) +
+    geom_point(aes(y=days, color=Location))+ylab("Maximum Water Level")+xlab("Days inundated")+ 
+    theme_bw()+
+    theme(axis.title.y = element_text(size=18,margin = margin(t = 0, r = 10, b = 0, l = 20)), 
+          axis.title.y.right = element_text(size=15,margin = margin(t = 0, r = 0, b = 0, l = 20)),
+          axis.title.x=element_text(size=18), axis.text.x=element_text(size=18),
+          axis.text.y = element_text(size=18),title =element_text(size=18, face='bold'),
+          strip.text.y = element_text(size = 18),
+          legend.text = element_text(size=12),
+          legend.title = element_text(size=12),
+          legend.position = "right")+ggtitle(p$wtr_yr)+
+    guides(fill=guide_legend(ncol=2))
+  print(q)
+}
 
 
+#Now get max water level ever
+max(vp2$Water_Level)
+vp2 %>% slice_max(Water_Level)
 
+vp3<-vp2 %>%
+  select(Water_Level, days, wtr_yr,Location,VP_Name) %>%
+  group_by(Location,VP_Name) %>%
+  filter(Water_Level==max(Water_Level)) %>%
+  ungroup()
+vp3<-vp3[vp3$Location!="storke_ranch",]
 
+vp4<-vp2 %>%
+  select(Water_Level, days, wtr_yr,Location) %>%
+  group_by(Location) %>%
+  filter(Water_Level==max(Water_Level)) %>%
+  ungroup()
+
+ggplot(vp3,aes(x=Water_Level)) +
+  geom_point(aes(y=days, shape=Location,color=wtr_yr,size=2),size=2)+ylab("Days inundated")+xlab("Maximum Water Level")+ 
+  theme_bw()+
+  theme(axis.title.y = element_text(size=18,margin = margin(t = 0, r = 10, b = 0, l = 20)), 
+        axis.title.y.right = element_text(size=15,margin = margin(t = 0, r = 0, b = 0, l = 20)),
+        axis.title.x=element_text(size=18), axis.text.x=element_text(size=18),
+        axis.text.y = element_text(size=18),title =element_text(size=18, face='bold'),
+        strip.text.y = element_text(size = 18),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=12),
+        legend.position = "right")+ggtitle("Maximum Water Level over all years")
+
+ggplot(vp3,aes(x=Water_Level)) +
+  geom_point(aes(y=days, color=wtr_yr,size=2),size=2)+ylab("Days inundated")+xlab("Maximum Water Level")+ 
+  theme_bw()+
+  theme(axis.title.y = element_text(size=18,margin = margin(t = 0, r = 10, b = 0, l = 20)), 
+        axis.title.y.right = element_text(size=15,margin = margin(t = 0, r = 0, b = 0, l = 20)),
+        axis.title.x=element_text(size=18), axis.text.x=element_text(size=18),
+        axis.text.y = element_text(size=18),title =element_text(size=18, face='bold'),
+        strip.text.y = element_text(size = 18),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=12),
+        legend.position = "right")+ggtitle("Maximum Water Level over all years")
